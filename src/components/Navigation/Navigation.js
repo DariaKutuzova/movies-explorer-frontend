c0e4aa67da048195624e6e034d6568e8b29931dd
DariaKutuzova <kutuzovad94@gmail.com>
2022-03-10 20:40:54 +0300

    feat: Мобильная версия доконца

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import '../Header/Header.css';
 import './Navigation.css';
-import {Link, useNavigate} from "react-router-dom";
+import {useLocation, Link, useNavigate} from "react-router-dom";
 
 function Navigation() {
 
     const navigate = useNavigate();
+    const address = useLocation();
 
     function handleAccount() {
         navigate('/profile');
@@ -13,9 +14,9 @@ function Navigation() {
     return (
         <nav className="navigation">
             <div className="navigation__links">
-                <Link className="navigation__link" to='/'>Главная</Link>
-                <Link className="navigation__link" to='/movies'>Фильмы</Link>
-                <Link className="navigation__link" to='/saved-movies'>Сохранённые фильмы</Link>
+                <Link className={`navigation__link ${address.pathname === '/' ? 'navigation__link_active' :''}`} to='/'>Главная</Link>
+                <Link className={`navigation__link ${address.pathname === '/movies' ? 'navigation__link_active' :''}`} to='/movies'>Фильмы</Link>
+                <Link className={`navigation__link ${address.pathname === '/saved-movies' ? 'navigation__link_active' :''}`} to='/saved-movies'>Сохранённые фильмы</Link>
             </div>
             <div className="header__account navigation__account" onClick={handleAccount}>
                 <a className="header__account-text">Аккаунт</a>
